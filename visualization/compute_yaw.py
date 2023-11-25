044c367164c90e36ea0a2f6363a6b5ef56083e7a
Ankit-Gupta-1511 <ankit.gupta.cs1511@gmail.com>
2023-11-25 16:29:19 +0530

    calculating yaw in a better way

--- visualization/compute_yaw.py
+++ visualization/compute_yaw.py
@@ -1,17 +1,10 @@
 import numpy as np
 
 def calc_theta_ray(img, prediction, camera_calib):
-        width = img.shape[1]
-        fovx = 2 * np.arctan(width / (2 * camera_calib[0][0]))
         center = (prediction['x1'] + prediction['x2']) / 2
-        dx = center - (width / 2)
-
-        mult = 1
-        if dx < 0:
-            mult = -1
-        dx = abs(dx)
-        angle = np.arctan( (2*dx*np.tan(fovx/2)) / width )
-        angle = angle * mult
+        u_distance = center - camera_calib[0, 2]
+        focal_length = camera_calib[0, 0]
+        angle = np.arctan(u_distance / focal_length)
 
         return angle
 
